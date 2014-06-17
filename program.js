90a975004e8985c35982d04c19c407599e9042e3
Mark Lybrand <mlybrand@gmail.com>
2014-06-17 15:27:33 -0700

    pre merge

--- program.js
+++ program.js
@@ -1,6 +1,20 @@
+if (typeof Object.create !== 'function') {
+	Object.create = function (o) {
+		var F = function () {};
+		F.prototype = o;
+		return new F();
+	};
+}
+
 Function.prototype.method = function(name, func) {
 	this.prototype[name] = func;
 	return this;
 }
 
 document.writeln("Hello, world!");
+
+document.writeln(foo());
+
+foo = function() {
+	return "bar";
+}
