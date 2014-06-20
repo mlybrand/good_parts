9c943b7ce41b3e659690239bfacad2b5f7000b58
Mark Lybrand <mlybrand@gmail.com>
2014-06-20 08:30:35 -0700

    still working....

--- program.js
+++ program.js
@@ -325,4 +325,41 @@ for (var i = 0; i <= 10; i++) {
 
 var factorial2 = memoizer([1,1], function(recur, n) {
 	return n * recur(n-1);
+});
+
+var Mammal = function(name) {
+	this.name = name;
 };
+Mammal.prototype.get_name = function() {
+	return this.name;
+};
+Mammal.prototype.says = function() {
+	return this.saying || '';
+};
+
+var myMammal = new Mammal('Herb the Mammal');
+var name = myMammal.get_name();
+
+var Cat = function(name) {
+	this.name = name;
+	this.saying = "meow";
+};
+Cat.prototype = new Mammal();
+Cat.prototype.purr = function(n) {
+	var i, s = '';
+	for(i = 0; i < n; i += 1) {
+		if (s) {
+			s += '-';
+		}
+		s += 'r';
+	}
+	return s;
+};
+Cat.prototype.get_name = function() {
+	return this.says() + ' ' + this.name + ' ' + this.says();
+}
+
+var myCat = new Cat('Henrietta');
+var says = myCat.says();
+var purr = myCat.purr(5);
+var name = myCat.get_name();
